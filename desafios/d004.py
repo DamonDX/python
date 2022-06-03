40d8fad62227b39c4c7264cf2ad4cddd3b6ee9be
Damon Souza <106456454+DamonDX@users.noreply.github.com>
2022-06-03 19:21:24 -0300

    Criação do d004

---
+++ desafios/d004.py
@@ -0,0 +1,35 @@
+n = input("digite algumas coisa:")
+
+print(type(n))
+print('print isalnum')
+print(n.isalnum())
+
+print('print isalpha')
+print(n.isalpha())
+
+print('print isascii')
+print(n.isascii())
+
+print('print isdecimal')
+print(n.isdecimal())
+
+print('print isdigit')
+print(n.isdigit())
+
+print('print isidentifier')
+print(n.isidentifier())
+
+print('print isnumeric')
+print(n.isnumeric())
+
+print('print islower')
+print(n.islower())
+
+print('print isprintable')
+print(n.isprintable())
+
+print('print isspace')
+print(n.isspace())
+
+print('print istitle')
+print(n.istitle())
