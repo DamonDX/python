de147574908593d8a02df2bb86921a5fdcaeaad4
Damon Souza <106456454+DamonDX@users.noreply.github.com>
2022-07-06 19:50:36 -0300

    apenas alguns comentarios

--- exercicios/mensagem_simple.py
+++ exercicios/mensagem_simple.py
@@ -1,3 +1,5 @@
+#Programa simples de mensagem
 mensagem_simples = str('Um passo de cada vez')
 
+#Printando a mensgam na tela
 print(mensagem_simples)
--- exercicios/name_cases.py
+++ exercicios/name_cases.py
@@ -1,13 +1,20 @@
+#salvando nomes de uma pessoa famosa
 nome = 'eric'
-
 nome_famoso = 'albert'
 sobrenome_famoso = 'einstein'
+
+#nome completo do famoso
 nome_completo_famoso = nome_famoso + " " + sobrenome_famoso
+
+#sitação
 sitacao = '"Uma pessoa que nunca cometeu um erro jamais tentou nada novo"'
 
+#Junato a sitação com o nome do famoso e printando em seguida
 message= nome_completo_famoso.title() + ' certa vez disse: ' +sitacao
 print('Albert Einstein certa vez disse: "Uma pessoa que nunca cometeu um erro jamais tentou nada novo"')
 print(message)
+
+#apenas alguns testes de strings
 print('Olá ' +nome+', você gostaria de aprender um pouco de Python hoje?')
 print('Olá ' +nome.lower()+', você gostaria de aprender um pouco de Python hoje?')
 print('Olá ' +nome.upper()+', você gostaria de aprender um pouco de Python hoje?')
@@ -16,3 +23,5 @@ nome_2 = '\n\tdamon '
 print(nome_2.lstrip())
 print(nome_2.rstrip())
 print(nome_2.strip())
+
+#Programa para mostrar a sitação de um famoso e alguns testes
